47d19bcd2094603f8c791155c1c5de8cceb41e14
liboru <1036513264@qq.com>
2021-04-27 19:39:51 +0800

    依赖查找demo

---
+++ ioc-container-overview/src/main/java/com/liboru/learn/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
@@ -0,0 +1,55 @@
+package com.liboru.learn.spring.ioc.overview.dependency.lookup;
+
+import com.liboru.learn.spring.ioc.overview.annotation.Super;
+import com.liboru.learn.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
+
+public class DependencyLookupDemo {
+
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context.xml");
+        /*lookupInRealTime(beanFactory);
+        lookupInLazy(beanFactory);*/
+        /*lookupByType(beanFactory);
+        lookupCollectionTypeByType(beanFactory);*/
+        lookupByAnnotationType(beanFactory);
+    }
+
+    private static void lookupByAnnotationType(BeanFactory beanFactory) {
+        if(beanFactory instanceof ListableBeanFactory){
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory)beanFactory;
+            Map<String, User> users = (Map)listableBeanFactory.getBeansWithAnnotation(Super.class);
+            System.out.println("根据注解查找"+users);
+        }
+    }
+
+    private static void lookupCollectionTypeByType(BeanFactory beanFactory) {
+        if(beanFactory instanceof ListableBeanFactory){
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory)beanFactory;
+            Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println(users);
+        }
+    }
+
+    private static void lookupByType(BeanFactory beanFactory) {
+        User user = beanFactory.getBean(User.class);
+        System.out.println("根据类型查找："+user);
+    }
+
+    private static void lookupInLazy(BeanFactory beanFactory){
+        ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
+        User user = objectFactory.getObject();
+        System.out.println("延迟查找："+user);
+    }
+
+    private static void lookupInRealTime(BeanFactory beanFactory){
+        User user = (User)beanFactory.getBean("user");
+        System.out.println("实时查找："+user);
+    }
+
+}
