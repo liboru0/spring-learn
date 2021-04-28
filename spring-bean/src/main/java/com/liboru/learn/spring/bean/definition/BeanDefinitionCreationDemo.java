841a22f98b99e2b0a06b85789f5ac30756bcd616
liboru <1036513264@qq.com>
2021-04-28 14:31:06 +0800

    BeanDefinition 构建

---
+++ spring-bean/src/main/java/com/liboru/learn/spring/bean/definition/BeanDefinitionCreationDemo.java
@@ -0,0 +1,34 @@
+package com.liboru.learn.spring.bean.definition;
+
+import com.liboru.learn.spring.ioc.overview.domain.User;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+
+public class BeanDefinitionCreationDemo {
+
+    public static void main(String[] args) {
+
+        // 1. 通过 BeanDefinitionBuilder 构建
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
+        // 通过属性设置
+        beanDefinitionBuilder
+                .addPropertyValue("id",1L)
+                .addPropertyValue("name","xiaoli");
+        // 获取 BeanDefinition 实例
+        BeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
+        // BeanDefinition 并非 Bean 终态，可以自定义修改
+
+        // 2. 通过 AbstractBeanDefinition 以及派生类
+        GenericBeanDefinition genericBeanDefinition = new GenericBeanDefinition();
+        genericBeanDefinition.setBeanClass(User.class);
+        MutablePropertyValues propertyValues = new MutablePropertyValues();
+        propertyValues
+                .add("id",2L)
+                .add("name","xiaowang");
+        genericBeanDefinition.setPropertyValues(propertyValues);
+
+    }
+
+}
