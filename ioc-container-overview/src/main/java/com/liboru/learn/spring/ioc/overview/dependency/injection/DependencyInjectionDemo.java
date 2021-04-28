b5a5cb1cd183b134f3de95536a6bcd40e96a00a7
liboru <1036513264@qq.com>
2021-04-28 11:03:39 +0800

    ApplicationContext 和 BeanFactory 的关系

--- ioc-container-overview/src/main/java/com/liboru/learn/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ ioc-container-overview/src/main/java/com/liboru/learn/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -14,27 +14,38 @@ import org.springframework.core.env.Environment;
 public class DependencyInjectionDemo {
 
     public static void main(String[] args) {
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
 
         // 依赖来源一：自定义 Bean
-        UserRepository userRepository = beanFactory.getBean("userRepository",UserRepository.class);
+        UserRepository userRepository = applicationContext.getBean("userRepository",UserRepository.class);
         System.out.println(userRepository);
 
         // 依赖来源二：依赖注入，内建依赖
-        System.out.println(userRepository.getBeanFactory());
-        System.out.println(userRepository.getBeanFactory()==beanFactory);
+        whoIsContainer(userRepository,applicationContext);
 
         ObjectFactory<ApplicationContext> userObjectFactory = userRepository.getObjectFactory();
         System.out.println(userObjectFactory.getObject());
-        System.out.println(userObjectFactory.getObject()==beanFactory);
+        System.out.println(userObjectFactory.getObject()==applicationContext);
 
         // 依赖查找（报错）
         //System.out.println(beanFactory.getBean(BeanFactory.class));
 
         // 依赖来源三：容器内建 Bean
-        Environment environment = beanFactory.getBean(Environment.class);
+        Environment environment = applicationContext.getBean(Environment.class);
         System.out.println(environment);
 
+    }
+
+    private static void whoIsContainer(UserRepository userRepository,ApplicationContext applicationContext){
+        /**
+         * userRepository.getBeanFactory() 通过类型依赖注入得到的Bean
+         *  得到的是 DefaultListableBeanFactory
+         *  因为在 AbstractApplicationContext#prepareBeanFactory 方法中
+         *      beanFactory.registerResolvableDependency(BeanFactory.class, beanFactory);
+         *      以上代码指定了 BeanFactory 类型的对象是 ApplicationContext#getBeanFactory() 方法的内容，而非它自身。
+         */
+        System.out.println(userRepository.getBeanFactory());
+        System.out.println(userRepository.getBeanFactory()==applicationContext);
     }
 
 
