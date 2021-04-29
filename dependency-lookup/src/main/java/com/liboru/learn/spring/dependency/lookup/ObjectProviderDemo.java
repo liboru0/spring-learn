c399849ba34937e4fc31f8beadefbf4083bf9a25
liboru <1036513264@qq.com>
2021-04-29 15:37:51 +0800

    getIfAvailable/stream

--- dependency-lookup/src/main/java/com/liboru/learn/spring/dependency/lookup/ObjectProviderDemo.java
+++ dependency-lookup/src/main/java/com/liboru/learn/spring/dependency/lookup/ObjectProviderDemo.java
@@ -1,8 +1,12 @@
 package com.liboru.learn.spring.dependency.lookup;
 
+import com.liboru.learn.spring.ioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import java.util.Iterator;
 
 public class ObjectProviderDemo {
 
@@ -14,9 +18,30 @@ public class ObjectProviderDemo {
         applicationContext.refresh();
 
         lookupByObjectProvider(applicationContext);
+        lookupIfAvailable(applicationContext);
+        lookupByStreamOps(applicationContext);
 
         applicationContext.close();
 
+    }
+
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+        /*Iterable<String> iterable = objectProvider;
+        for (String s : iterable) {
+            System.out.println(s);
+        }*/
+        objectProvider.stream().forEach(System.out::println);
+    }
+
+    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> beanProvider = applicationContext.getBeanProvider(User.class);
+        User user = beanProvider.getIfAvailable(() -> {
+            User userIn = new User();
+            userIn.setName("default");
+            return userIn;
+        });
+        System.out.println(user);
     }
 
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
@@ -26,8 +51,14 @@ public class ObjectProviderDemo {
     }
 
     @Bean
+    @Primary
     public String helloWorld(){
         return "Hello,world";
+    }
+
+    @Bean
+    public String message(){
+        return "Message";
     }
 
 }
